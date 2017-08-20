14a56374b599cced5568edf56fd2c1e8cf1114ce
Ivan Danov <idanov@users.noreply.github.com>
2017-08-20 18:16:16 +0100

    Implement optimize and pass the test

--- main.py
+++ main.py
@@ -110,8 +110,11 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :param num_classes: Number of classes to classify
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
-    # TODO: Implement function
-    return None, None, None
+    # DONE: Implement function
+    logits = tf.nn.sigmoid(nn_last_layer, name='logits')
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=correct_label, logits=logits))
+    train_op = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy_loss)
+    return tf.reshape(logits, [-1, num_classes]), train_op, cross_entropy_loss
 tests.test_optimize(optimize)
 
 
