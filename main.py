80d86afa2d7b6d91d31393418d22ed52b9130138
Ivan Danov <idanov@users.noreply.github.com>
2017-08-29 17:06:56 +0100

    Remove the sigmoid from the reshaped last layer output

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # DONE: Implement function
-    logits = tf.nn.sigmoid(tf.reshape(nn_last_layer, (-1, num_classes)), name='logits')
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
     labels = tf.reshape(correct_label, (-1, num_classes))
     cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=logits))
     train_op = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy_loss)
